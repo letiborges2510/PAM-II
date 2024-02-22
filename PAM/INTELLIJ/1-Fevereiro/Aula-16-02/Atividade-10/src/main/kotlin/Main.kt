39f65b9d4064384a59b01c780c0d10ace94f25bf
Letícia Borges <99838964+letiborges2510@users.noreply.github.com>
2024-02-22 16:29:03 -0300

    Add files via upload

---
+++ PAM/INTELLIJ/1-Fevereiro/Aula-16-02/Atividade-10/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package fundamentos
+
+fun main ( args: Array <String>) {
+
+    val num1 Int = 2
+    val num2 Int = 3
+    val maiorValor = if (num1  > 2 ) {
+
+        println ("processando...")
+        num1
+    }else {
+        println ("processando...")
+        num2
+
+    }
+    println ("O maior valor é $maiorValor")
+
+}
---
+++ PAM/INTELLIJ/1-Fevereiro/Aula-16-02/Atividade-8/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package fundamentos
+
+fun main ( args: Array <String>) {
+
+    val nota: Double = 8.3
+    if ( nota >= 7) {
+        println ( "Aprovado")
+
+    }
+
+}
